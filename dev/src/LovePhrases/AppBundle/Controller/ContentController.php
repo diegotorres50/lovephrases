19f4f3dbefd1ab92f99459e70a34d5858ba41aee
Diego Torres <diegotorres50@hotmail.com>
2015-08-20 13:33:14 -0500

    despliegue de articulo

--- dev/src/LovePhrases/AppBundle/Controller/ContentController.php
+++ dev/src/LovePhrases/AppBundle/Controller/ContentController.php
@@ -446,4 +446,141 @@ class ContentController extends Controller
         //
         //return $response; @diegotorres50 use esto si usa JsonResponse()
     }
+
+     /**
+     * @Route("/content/article/{id}", defaults={"id" = 1}, name="lovephrases_content_article") 
+     */
+    public function searchAction(Request $request, $id)
+    {
+
+        $thereIsError = FALSE;
+        $errorsList = array();
+
+
+        //Defini el array del json
+        $data = array();
+
+        $datetime = new \DateTime("now");
+
+        $data['datetime'] = $datetime->format('Y-m-d H:i:s');
+        $data['type'] = 'article';
+        $data['alias'] = 'love-phrases';
+        $data['tag'] = 'love-phrases'; 
+        $data['label'] = 'Love Phrase';
+        $data['path'] = '/';
+
+        //Instanciamos el modelo de conexion mysql usando el modelo de conexion
+        $m = new Model(
+            $this->container->getParameter('database_name'), 
+            $this->container->getParameter('database_user'),
+            $this->container->getParameter('database_password'),
+            $this->container->getParameter('database_host')
+        );
+
+        //Seteamos en un array los parametros del query para mysql
+        $values = array();
+
+        //Seteamos que tabla vamos a consultar
+        $values['TABLE'] = 'sections';
+
+        //Seteamos los alias de los campos de deseamos traer en el query y un lbl para la vista
+        $values['FIELDS'] = array( //identificador_campo => aliascustomizado_campo
+            'section_alias' => 'alias',
+            'section_name' => 'name',
+            'section_path' => 'path'
+            );
+
+        //Seteamos el ordenamiento por defecto del query
+        $values['ORDER_BY'] = array( 
+            'section_order asc' //ordenar por id para traerlo alfabeticamente
+            );
+
+        //Seteamos el paginador de la consulta para mysql
+        $values['LIMIT'] = array( 
+            'OFFSET' => 0, //Desde la fila que se desea mostrar. Esto es un parametro de la ruta
+            'ROW_COUNT' => 1000 //Cantidad maximo de registros a traer. Esto es un parametro de la ruta
+            );
+
+        //Tratamos de consultar la lista de usuarios en la tabla de mysql
+        $getSections = $m->getDataFromSingleTable($values);
+
+        //Si el query falla mostramos un error
+        if (!empty($getSections) && is_array($getSections) && isset($getSections['errorMsg'])) {
+            $thereIsError = TRUE;
+            $errorsList[]['error'] = $getSections['errorMsg'];
+        } else if (isset($getSections['rows_found']) && empty($getSections['rows_found'])) { //Si no hay registros, mostramos un warning
+            $thereIsError = TRUE;
+            $errorsList[]['error'] = 'No sections';
+        } else $data['sections'] = $getSections['rows_found'];
+
+        //Tratamos de consultar la lista de articulos en la tabla de mysql
+        $getUniqueArticle = $m->getUniqueArticle($id);
+
+        //var_dump($getHomePrimaryArticles); exit;
+
+        //Si el query falla mostramos un error
+        if (!empty($getUniqueArticle) && is_array($getUniqueArticle) && isset($getUniqueArticle['errorMsg'])) {
+            $thereIsError = TRUE;
+            $errorsList[]['error'] = $getUniqueArticle['errorMsg'];
+        } else if (isset($getUniqueArticle['rows_found']) && empty($getUniqueArticle['rows_found'])) { //Si no hay registros, mostramos un warning
+            $thereIsError = TRUE;
+            $errorsList[]['error'] = 'No article';
+        } else  {
+
+            //$data['articles'] = $getHomePrimaryArticles[0]; //ojo, el array que devuelve la consulta no debe contener ni un solo valor nulo pues un valor nulo anula todo el array 
+
+            $_articles = array();
+
+            $_i = 0;
+
+            foreach ($getUniqueArticle[0] as $article) {
+                //Tratamos de consultar la lista de parametros por articulo en la tabla de mysql
+                $_articles[$_i] = $article;
+                $_parameter = $m->getArticleParameters($article['article_id']);  //OJO FALTA VALIDAR EL RESULTADO DE LA FUNCION  
+                $_tag = $m->getArticleTags($article['article_id']);  //OJO FALTA VALIDAR EL RESULTADO DE LA FUNCION     
+                $_articles[$_i]['parameters'] = $_parameter[0];
+                $_articles[$_i]['tags'] = $_tag[0];
+                $_i ++;        
+            }   
+            
+            $data['articles'] = $_articles;
+
+        }
+
+        //
+        if($thereIsError) $data = $errorsList;
+
+        //JSON response: http://symfony.com/doc/current/components/http_foundation/introduction.html   
+        //@diegotorres50 prefiero no usar este metodo porque me ponen textos de cabacere, usare json_encode() 
+        //$response = new JsonResponse();
+        //$response->setData($data);
+
+        //Codificamos el array en formato json
+        $response = json_encode($data);
+
+        //Si en la url llega el parametro con el valor 'production' generamos el archivo fisico del feed
+        /*
+        if($var == 'production') {
+            // 
+            //Preparamos el filesystem para guardar el json en un archivo fisico
+            $fs = new Filesystem();
+
+            //Ruta del directorio de contenido
+            $content_path = $this->container->getParameter('kernel.root_dir') . '/../web/bundles/lovephrasesapp/content/love-phrases/' . $alias . '/';
+
+            //Nombre del archivo home
+            $json_file = 'home.json';
+
+            try {
+                //Con el metodo dumpfile() guardamos el responseen un archivo
+                $fs->dumpFile($content_path . $json_file, $response);
+            } catch (IOExceptionInterface $e) {
+                echo "An error occurred while creating your directory at ".$e->getPath();
+            }
+        }
+        */    
+        return new Response($response);
+        //
+        //return $response; @diegotorres50 use esto si usa JsonResponse()
+    }    
 }
--- dev/src/LovePhrases/AppBundle/Controller/DefaultController.php
+++ dev/src/LovePhrases/AppBundle/Controller/DefaultController.php
@@ -121,4 +121,65 @@ class DefaultController extends Controller
         return $this->render('LovePhrasesAppBundle:Default:landingtag.html.twig', array('data' => $json));
     }
 
+    /**
+     * @Route(
+     *     "/search",
+     *     name= "lovephrases_default_search"
+     * )
+     */    
+    public function searchAction(Request $request)
+    {
+        // retrieve GET and POST variables respectively
+        $query_s = $request->query->get('s');
+        //$request->request->get('bar', 'default value if bar does not exist');
+        //
+        
+        $query_id = $request->query->get('id');
+
+        $domain = $_SERVER['HTTP_HOST'];
+        $prefix = 'http://';
+
+        if (isset($query_id) && !empty($query_id)) {
+
+            $path = $prefix . $domain . '/content/article/'. $query_id;
+
+        }    
+
+        //@diegotorres50: los json al vuelo no se pueden tratar con file_get_contents(), en vez de eso se debe usar curl de php cuando no hay archivo fisico
+
+        //$content = file_get_contents($path);
+        //$json = json_decode($content, true);
+
+        // Crea un nuevo recurso cURL
+        $ch = curl_init();
+
+        // Establece la URL y otras opciones apropiadas
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Esto evita que la salida se transfiera directamente al navegador y asi controlar todo en una variable posteriortemente
+
+        // Captura la URL y la envía al navegador
+        $_json = curl_exec($ch);
+
+        $json = json_decode($_json, true);
+
+        // Cierrar el recurso cURLy libera recursos del sistema
+        curl_close($ch);
+
+        //var_dump($json); exit;
+
+
+
+        /*
+        $path = 'bundles/lovephrasesapp/content/love-phrases/' . $section . '/home.json';
+        $content = file_get_contents($path);
+        $json = json_decode($content, true);
+
+        //var_dump($json); exit;
+        */
+       
+       //var_dump($json); exit;
+        return $this->render('LovePhrasesAppBundle:Default:article.html.twig', array('data' => $json));
+        
+    }
 }
